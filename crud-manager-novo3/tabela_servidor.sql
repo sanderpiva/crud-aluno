ca614d6575760678e24f1a715b854e49c52ddc50
Sander Gustavo Piva <sanderpiva38@gmail.com>
2023-05-27 17:43:58 -0300

    appServidor1a

---
+++ crud-manager-novo3/tabela_servidor.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS servers(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(150) NOT NULL,
+    endereco VARCHAR(150) NOT NULL,
+    telefone VARCHAR(150) NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    company_id INT(11) NOT NULL
+);
